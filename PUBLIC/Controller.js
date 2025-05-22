a2ace35d7374f3282c1818cc30e7b9fb770d51c7
VargaDoro <varga.dorothea.timea@gmail.com>
2025-05-22 19:38:26 +0200

    hibás

--- PUBLIC/Controller.js
+++ PUBLIC/Controller.js
@@ -13,6 +13,18 @@ class Controller {
       this.initEventListeners();
       eventListenersRegistered = true;
     }
+    //atraktam konstruktorba a szures és rendezést mivel azt minden elemre akarom használni nemcsak egysze
+    window.addEventListener("rendezes", (event) => {
+      const { irany } = event.detail;
+      this.model.rendezTermekLista(irany);
+      this.termekek.init(this.model.getTermekLista());
+    });
+
+    window.addEventListener("szures", (event) => {
+      const { keresesoKifejezes } = event.detail;
+      const szurtLista = this.model.szuresTermekLista(keresesoKifejezes);
+      this.initTermekek(szurtLista);
+    });
 
     this.kosarDBElem = document.querySelector("#kosarDb");
     this.kosarDbKiir();
@@ -59,35 +71,23 @@ class Controller {
         console.error("Termék nem található:", id);
       }
     });
-
+    
     window.addEventListener("novel", (event) => {
       const id = event.detail;
       this.model.increaseQuantity(id);
       this.initKosar(); // új hívás
     });
-    
+
     window.addEventListener("csokkent", (event) => {
       const id = event.detail;
       this.model.decreaseQuantity(id);
       this.initKosar();
     });
-    
+
     window.addEventListener("torles", (event) => {
       const id = event.detail;
       this.model.removeKosarItem(id);
       this.initKosar();
-    });
-
-    window.addEventListener("rendezes", (event) => {
-      const { irany } = event.detail;
-      this.model.rendezTermekLista(irany);
-      this.termekek.init(this.model.getTermekLista());
-    });
-
-    window.addEventListener("szures", (event) => {
-      const { keresesoKifejezes } = event.detail;
-      const szurtLista = this.model.szuresTermekLista(keresesoKifejezes);
-      this.termekek.init(szurtLista);
     });
   }
 }
--- teszt/KosarTeszt.js
+++ teszt/KosarTeszt.js
@@ -1,13 +1,9 @@
 import Kosar from "../PUBLIC/Kosar.js";
+/* kosra osztály annyi táblázat sort jelenit-e meg ahány eleme van a lsitánknak?
+    az oldalon a kosárban lévő terméknév helyesen jelenik-e meg? */
 
-/* kosar osztály tesztelése:
-   - A megjelenített <tr> sorok száma helyes
-   - A termék nevei helyesen jelennek meg
-   - A mennyiség helyesen jelenik meg a megfelelő cellákban
-*/
-
-function kosarMegjelenitTeszt() {
-  const szElem = document.createElement("div");
+function kosarMejelenitTeszt() {
+  const szELem = document.createElement("div");
   const tesztLista = [
     {
       id: 0,
@@ -15,7 +11,7 @@ function kosarMegjelenitTeszt() {
       ar: 1000,
       kep: "./kepek/placeholder.jpg",
       leiras: "Ez egy példa termék leírása.",
-      mennyiseg: 3,
+      mennyiseg: 2,
     },
     {
       id: 1,
@@ -23,41 +19,30 @@ function kosarMegjelenitTeszt() {
       ar: 1300,
       kep: "./kepek/placeholder.jpg",
       leiras: "Ez egy példa termék leírása.",
-      mennyiseg: 5,
+      mennyiseg: 1,
     },
   ];
-
-  // Kosár példány létrehozása
-  const kosar = new Kosar(tesztLista, szElem);
-
-  // 1. Teszt: <tr> sorok száma (fejléc + elemek)
-  const sorLista = szElem.querySelectorAll("tr");
+  const kosar = new Kosar(tesztLista, szELem);
+  const sorLista = szELem.querySelectorAll("tr");
+  console.log(sorLista);
   console.assert(
     sorLista.length === tesztLista.length + 1,
-    `Hiba: ${sorLista.length} sor van, de ${tesztLista.length + 1} kellett volna!`
+    "Hiba, nem stimmel a tr tagek száma"
   );
 
-  // 2. Teszt: első termék neve
-  const tdNev1 = szElem.querySelector("table tbody tr:nth-child(1) td:nth-child(2)");
+  /* az első igazi nem fejléces td tagjében jó név szerepel-e */
+
+  const tdNev = szELem.querySelector(
+    "table tbody tr:nth-child(1) td:nth-child(2)"
+  );
+  console.log(tdNev, tdNev.innerHTML);
   console.assert(
-    tdNev1.innerHTML === tesztLista[0].nev,
-    `Hiba: az első termék neve '${tdNev1.innerHTML}', elvárt: '${tesztLista[0].nev}'`
+    tdNev.innerHTML === tesztLista[0].nev,
+    "Hiba: nem megfelelő név jelenik meg"
   );
 
-  // 3. Teszt: mennyiségek megjelenése
-  const tdMennyiseg1 = szElem.querySelector("table tbody tr:nth-child(1) td:nth-child(4)");
-  const tdMennyiseg2 = szElem.querySelector("table tbody tr:nth-child(2) td:nth-child(4)");
-
-  console.assert(
-    tdMennyiseg1.textContent.includes(tesztLista[0].mennyiseg.toString()),
-    `Hiba: első termék mennyisége hibás (${tdMennyiseg1.textContent})`
-  );
-  console.assert(
-    tdMennyiseg2.textContent.includes(tesztLista[1].mennyiseg.toString()),
-    `Hiba: második termék mennyisége hibás (${tdMennyiseg2.textContent})`
-  );
-
-  console.log("kosarMegjelenitTeszt() lefutott");
+  
+  console.log("lefutott a kosarMegjelenitTeszt");
 }
 
-kosarMegjelenitTeszt();
+kosarMejelenitTeszt();
