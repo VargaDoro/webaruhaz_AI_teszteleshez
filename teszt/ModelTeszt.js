32da519261c113e8266db78726b8a1045139d1b9
VargaDoro <varga.dorothea.timea@gmail.com>
2025-05-22 15:52:18 +0200

    decrease/increase fv

--- teszt/ModelTeszt.js
+++ teszt/ModelTeszt.js
@@ -23,7 +23,6 @@ function addKosarTeszt_AzAdottTermekMegNemSzerepelAKosarban() {
   );
   console.log("addKosarTeszt_AzAdottTermekMegNemSzerepelAKosarban lefutott");
 }
-
 addKosarTeszt_AzAdottTermekMegNemSzerepelAKosarban();
 
 function addKosarTeszt_AzAdottTermekMarSzerepelAKosarban() {
@@ -48,7 +47,6 @@ function addKosarTeszt_AzAdottTermekMarSzerepelAKosarban() {
   );
   console.log("addKosarTeszt_AzAdottTermekMarSzerepelAKosarban lefutott");
 }
-
 addKosarTeszt_AzAdottTermekMarSzerepelAKosarban();
 
 function removeKosarItemTeszt() {
@@ -63,10 +61,67 @@ function removeKosarItemTeszt() {
   };
   modell.addKosar(termek);
   termek.mennyiseg = 1;
-  modell.removeKosarItem(termek.id)
+  modell.removeKosarItem(termek.id);
   const kosarbanlevotermek = modell.getKosarLista();
-  console.assert(modell.getKosarLista().length === 0, "Hiba: a termék nem került eltávolításra")
-  console.log("removeKosarItemTeszt lefutott")
+  console.assert(
+    modell.getKosarLista().length === 0,
+    "Hiba: a termék nem került eltávolításra"
+  );
+  console.log("removeKosarItemTeszt() lefutott");
+}
+removeKosarItemTeszt();
+
+function increaseQuantityTeszt() {
+  const modell = new Model();
+  const termek = {
+    id: 0,
+    nev: "Termék 1",
+    ar: 1000,
+    kep: "./kepek/placeholder.jpg",
+    leiras: "Ez egy példa termék leírása.",
+  };
+  modell.addKosar(termek);
+  modell.increaseQuantity(termek.id);
+  const kosarbanlevotermek = modell.getKosarLista()[0];
+  console.assert(
+    kosarbanlevotermek.mennyiseg === 2,
+    `Hiba: a mennyiség nem nőtt! Jelenlegi érték: ${kosarbanlevotermek.mennyiseg}`
+  );
+  console.assert(
+    modell.getKosarLista().length === 1,
+    "Hiba: a kosárban duplikálódott a termék!"
+  );
+  console.log("increaseQuantityTeszt() lefutott");
+}
+increaseQuantityTeszt()
+
+function decreaseQuantityTeszt() {
+  const modell = new Model();
+  const termek = {
+    id: 0,
+    nev: "Termék 1",
+    ar: 1000,
+    kep: "./kepek/placeholder.jpg",
+    leiras: "Ez egy példa termék leírása.",
+  };
+  modell.addKosar(termek); 
+  modell.addKosar(termek); 
+  modell.decreaseQuantity(termek.id);
+  const kosar = modell.getKosarLista();
+  console.assert(
+    kosar.length === 1,
+    "Hiba: a termék eltávolításra került, pedig csak csökkennie kellett volna!"
+  );
+  console.assert(
+    kosar[0].mennyiseg === 1,
+    `Hiba: a mennyiség nem csökkent 1-re! (${kosar[0].mennyiseg})`
+  );
+  modell.decreaseQuantity(termek.id);
+  console.assert(
+    modell.getKosarLista().length === 0,
+    "Hiba: a termék nem került eltávolításra, amikor a mennyiség 1 volt!"
+  );
+  console.log("decreaseQuantityTeszt() lefutott");
 }
 
-removeKosarItemTeszt()
+decreaseQuantityTeszt()
